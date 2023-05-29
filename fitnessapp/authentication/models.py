55e89a9e5d1f2c30c0ad82a3efac132033c0037e
Tommaso-Bev <tommaso.bevilacqua@stud.unifi.it>
2023-05-29 12:25:02 +0200

    login, logout and added workouts from database

--- fitnessapp/authentication/models.py
+++ fitnessapp/authentication/models.py
@@ -4,7 +4,8 @@ class workout(models.Model):
    workout_name=models.CharField(max_length=25, unique=True)
    muscle_plan=models.CharField(max_length=25)
    description=models.CharField(max_length=300)
-
+   image=models.ImageField(upload_to='img')
+   
 class workout_plan(models.Model):
    username=models.CharField(max_length=128, unique=True)
    wourkout_name=models.CharField(max_length=25)
--- fitnessapp/fitnessapp/urls.py
+++ fitnessapp/fitnessapp/urls.py
@@ -21,3 +21,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include("authentication.urls"))
 ]
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
