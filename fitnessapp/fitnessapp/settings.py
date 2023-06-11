77becfa1f7818e3b506ffbdc04d2023a7bbfe9f7
Tommaso-Bev <tommaso.bevilacqua@stud.unifi.it>
2023-06-11 12:18:41 +0200

    I'm trying to deploy, wish me luck

--- fitnessapp/fitnessapp/settings.py
+++ fitnessapp/fitnessapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure--ngkxf+2h89ad4g2bbo&vvzbz*ewe7nrw)lbvfaxrr4w0+$(wo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
