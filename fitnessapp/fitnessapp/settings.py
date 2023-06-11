a664fd8ae3d3193b1467fbc387fcef6aa1b7b3dc
Tommaso-Bev <tommaso.bevilacqua@stud.unifi.it>
2023-06-11 17:04:00 +0200

    Trying to fix the login form

--- fitnessapp/fitnessapp/settings.py
+++ fitnessapp/fitnessapp/settings.py
@@ -102,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -134,3 +133,6 @@ STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+CSRF_TRUSTED_ORIGINS = ['example.com']
